1481f1cc1c8572c347e62562dbe9a64446e2142e
Stuart Littlefair <s.littlefair@shef.ac.uk>
2017-10-23 14:10:07 +0100

    python2 compat for socketserver

--- hcam_drivers/utils/rtplot.py
+++ hcam_drivers/utils/rtplot.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, unicode_literals, absolute_import, division
-import socketserver
+import six
+from six.moves import socketserver
 import http.server
 import socket
 import errno
