3081b14b72b083e02a88366c67454da1dc1aab80
Stuart Littlefair <s.littlefair@shef.ac.uk>
2017-10-25 11:40:38 +0100

    symbol font

--- hcam_drivers/utils/tkutils.py
+++ hcam_drivers/utils/tkutils.py
@@ -35,25 +35,46 @@ def addStyle(w):
         widget element to style
     """
     # access global container in root widget
-    g = get_root(w).globals
+    root = get_root(w)
+    g = root.globals
 
     # Default font
-    g.DEFAULT_FONT = font.nametofont("TkDefaultFont")
-    g.DEFAULT_FONT.configure(size=10, weight='bold')
+    fam_name_list = [fam for fam in font.families() if 'symbol' in fam.lower()]
+    if len(fam_name_list):
+        font_dict = {'family': fam_name_list[0],
+                     'weight': 'bold',
+                     'size': 10}
+        g.DEFAULT_FONT = font.Font(w, **font_dict)
+    else:
+        print('falling back to defaults')
+        g.DEFAULT_FONT = font.nametofont("TkDefaultFont")
+        g.DEFAULT_FONT.configure(size=10, weight='bold')
     w.option_add('*Font', g.DEFAULT_FONT)
 
     # Menu font
-    g.MENU_FONT = font.nametofont("TkMenuFont")
-    g.MENU_FONT.configure(size=10)
+    if len(fam_name_list):
+        font_dict = {'family': fam_name_list[0],
+                     'weight': 'normal',
+                     'size': 10}
+        g.MENU_FONT = font.Font(w, **font_dict)
+    else:
+        g.MENU_FONT = font.nametofont("TkMenuFont")
+        g.MENU_FONT.configure(size=10)
     w.option_add('*Menu.Font', g.MENU_FONT)
 
     # Entry font
-    g.ENTRY_FONT = font.nametofont("TkTextFont")
-    g.ENTRY_FONT.configure(size=10)
+    if len(fam_name_list):
+        font_dict = {'family': fam_name_list[0],
+                     'weight': 'normal',
+                     'size': 10}
+        g.ENTRY_FONT = font.Font(w, **font_dict)
+    else:
+        g.ENTRY_FONT = font.nametofont("TkTextFont")
+        g.ENTRY_FONT.configure(size=10)
     w.option_add('*Entry.Font', g.ENTRY_FONT)
 
     # position and size
-    #    root.geometry("320x240+325+200")
+    # root.geometry("320x240+325+200")
 
     # Default colours. Note there is a difference between
     # specifying 'background' with a capital B or lowercase b
