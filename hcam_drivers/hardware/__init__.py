477f6c3b25de15248063ab9a91f50c9b92b1da50
Stuart Littlefair <s.littlefair@shef.ac.uk>
2018-02-02 21:14:15 +0000

    allow lower flow rates

--- hcam_drivers/hardware/__init__.py
+++ hcam_drivers/hardware/__init__.py
@@ -403,7 +403,7 @@ class CCDInfoWidget(tk.Toplevel):
 
             self.ccd_flow_rates.append(
                 FlowRateWidget(self.flow_frm, honey_ip, pen_address, name, update_interval,
-                               1.0, 10)
+                               0.85, 10)
             )
             self.ccd_flow_rates[-1].grid(
                     row=int(iccd/3), column=iccd % 3, padx=5, sticky=tk.W
