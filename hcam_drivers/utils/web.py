06156772da7dde124fa03e33d9ac0ce4bbe33cb4
Stuart Littlefair <s.littlefair@shef.ac.uk>
2018-02-02 15:52:14 +0000

    change framesize calcs back

--- hcam_drivers/utils/web.py
+++ hcam_drivers/utils/web.py
@@ -79,7 +79,7 @@ class FastFITSPipe:
     @lazyproperty
     def framesize(self):
         nsamps = self.hdr.get('ESO DET NSAMP', 1)
-        size = self.hdr['ESO DET ACQ1 WIN NX'] * self.hdr['ESO DET ACQ1 WIN NY'] // nsamps
+        size = self.hdr['ESO DET ACQ1 WIN NX'] * self.hdr['ESO DET ACQ1 WIN NY']
         bitpix = self.hdr['BITPIX']
         size = abs(bitpix) * size // 8
         # currently metadata consists of 36 bytes per frame (for timestamp)
