c83ccf790a94ba09a2d53f666c118e9b06fabbbf
Stuart Littlefair <s.littlefair@shef.ac.uk>
2018-02-14 22:33:36 +0000

    don't override GPS hardware flag in Idle mode

--- hcam_drivers/utils/obsmodes.py
+++ hcam_drivers/utils/obsmodes.py
@@ -236,7 +236,6 @@ class Idle(ObsMode):
         super(Idle, self).__init__(setup_data)
         self.detpars['DET.GPS'] = 'F'
         self.readoutMode = 1  # FF, Slow
-        self.use_gps_hardware = 0
         self.setup_acq_task(nq=5)
 
 
