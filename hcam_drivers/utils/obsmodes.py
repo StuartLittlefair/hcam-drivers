cb326055b3139853b02cbbd49b4261932a4dea01
Stuart Littlefair <s.littlefair@shef.ac.uk>
2018-05-15 13:29:39 +0100

    Revert "Revert "add program id and obid for GTC""
    This reverts commit eae42fcb6649e70a310fa773d5ab3868459078e6.

--- hcam_drivers/utils/obsmodes.py
+++ hcam_drivers/utils/obsmodes.py
@@ -97,9 +97,13 @@ class ObsMode(object):
             userpars.extend(
                 [(item, gtc_header_info[item]) for item in gtc_header_info]
             )
+            userpars.extend([
+                ('GTCPRGID', user_data.get('ID', '')),
+                ('GTCOBID', user_data.get('OBID', '1'))
+            ])
 
         # now update with GUI values. Do this after to allow override of
-        # GTC telescope server headers
+        # GTC telescope server headers
         userpars.extend([
             ('OBSERVER', user_data.get('Observers', '')),
             ('OBJECT', obj),
