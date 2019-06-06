6ed36c5d2e559c5b5f5b4fd566c699176e4c20ef
Stuart Littlefair <s.littlefair@shef.ac.uk>
2019-06-06 10:19:25 +0100

    fixes #144

--- hcam_drivers/hardware/rack.py
+++ hcam_drivers/hardware/rack.py
@@ -1,36 +1,15 @@
 # module to talk to the arduino in the thermal enclosure
-from hcam_widgets import DriverError
-from six.moves.urllib.error import URLError
-from six.moves.urllib.request import urlopen
+from hcam_widgets.gtc.corba import get_telescope_server
 
 
 class GTCRackSensor(object):
-    def __init__(self, address):
-        self.url = 'http://{}/'.format(address)
-
-    def __call__(self):
-        try:
-            resp = urlopen(self.url, timeout=5)
-            try:
-                status = resp.status
-            except AttributeError:
-                status = resp.getcode()
-            if status != 200:
-                raise URLError('response code from rack sensor NOK')
-            html = resp.read().decode()
-            fields = html.split('">')
-            temp = float(fields[1].split(' ')[0])
-            hum = float(fields[2].split(' ')[0])
-            return temp, hum
-        except URLError as err:
-            raise DriverError(str(err))
 
     @property
     def temperature(self):
-        t, h = self.__call__()
-        return t
+        s = get_telescope_server()
+        return s.getCabinetTemperature1()
 
     @property
     def humidity(self):
-        t, h,  = self.__call__()
-        return h
+        s = get_telescope_server()
+        return s.getHumidity()
