d1160215b16416c691d56d0ee90413f8704c404f
Stuart Littlefair <s.littlefair@shef.ac.uk>
2019-07-07 19:05:22 +0100

    dont use properties because of issues with dynamic lookup

--- hcam_drivers/hardware/rack.py
+++ hcam_drivers/hardware/rack.py
@@ -3,12 +3,11 @@ from hcam_widgets.gtc.corba import get_telescope_server
 
 
 class GTCRackSensor(object):
-    @property
+
     def temperature(self):
         s = get_telescope_server()
         return s.getCabinetTemperature1()
 
-    @property
     def humidity(self):
         s = get_telescope_server()
         return s.getHumidity()
